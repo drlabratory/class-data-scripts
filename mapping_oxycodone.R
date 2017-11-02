b22d6c42fb18d80faa2a16f9f61378579bdfa79d
Jeramia Ory <jeramia.ory@gmail.com>
2017-11-02 12:18:08 -0500

    Switched so declare column names using filter(), and added the ability to plot by Quarter(or a list of Quarters)

--- mapping_oxycodone.R
+++ mapping_oxycodone.R
@@ -1,48 +1,57 @@
 require(tidycensus) 
 require(tidyverse)
 require(maps)
+require(scales)
 
-census_api_key("b851144a625a1ff4d8e04253547dde222768dd39", install = "TRUE")
+census_api_key("b851144a625a1ff4d8e04253547dde222768dd39")
 
 # Mapping Oxycontin Prescriptions
 
-plot_drug <- function(dat, drugname, norm = TRUE) {
+plot_drug <- function(dat, drugname, norm = TRUE, limits = NULL, QuarterNum = c(1,2,3,4)) {
   # take a data frame, search for a drug, and plot it, 
   # either raw or normalized 
   dat <- dat %>% rename_all(make.names)
+  dat$Number.of.Prescriptions <- as.numeric(dat$Number.of.Prescriptions)
   data_fuzzy <- dat %>% 
-    select(State, Product.Name, Number.of.Prescriptions, Suppression.Used) %>% 
-    filter(grepl(pattern = drugname,x = Product.Name, ignore.case = TRUE), Suppression.Used == "false") %>% 
+    select(State, Product.Name, Number.of.Prescriptions, Suppression.Used, Quarter) %>% 
+    filter(grepl(pattern = drugname,x = Product.Name, ignore.case = TRUE), 
+           Suppression.Used == "false", Quarter %in% QuarterNum)  %>% 
     group_by(State) %>% summarise(drug_num = sum(Number.of.Prescriptions))
-  colnames(data_fuzzy) <- tolower(colnames(data_fuzzy))
-  data_fuzzy$state <-  state.name[match(data_fuzzy$state, state.abb)]
-  colnames(data_fuzzy)[1] <- "NAME"
+  View(data_fuzzy)
+  data_fuzzy$State <-  state.name[match(data_fuzzy$State, state.abb)]
   state_2010 <- get_decennial(geography = "state", variables = "P0010001", year = "2010")
-  data_fuzzy <- left_join(data_fuzzy, state_2010, by = 'NAME')
-  data_fuzzy$NAME <-  tolower(data_fuzzy$NAME)
-  data_fuzzy <- data_fuzzy %>% filter(!is.na(NAME))
-  data_fuzzy$value <- as.numeric(data_fuzzy$value)
-  data_fuzzy <- mutate(data_fuzzy, drug_norm = drug_num / value)
+  state_2010 <- state_2010 %>% 
+    select(
+      State = NAME,
+      Population = value
+    )
+  data_fuzzy <- left_join(data_fuzzy, state_2010, by = 'State')
+  data_fuzzy$State <-  tolower(data_fuzzy$State)
+  data_fuzzy <- data_fuzzy %>% filter(!is.na(State))
+  data_fuzzy$Population <- as.numeric(data_fuzzy$Population)
+  data_fuzzy <- mutate(data_fuzzy, drug_norm = drug_num / Population)
   mapping <- map_data("state")
   if (norm) {
     plot <- ggplot(data_fuzzy, aes(fill = drug_norm))
     # the map = mapping makes sure ggplot knows what to use
-    plot + geom_map(aes(map_id = NAME), map = mapping) + 
+    plot + geom_map(aes(map_id = State), map = mapping) + 
       expand_limits(x=mapping$long, y=mapping$lat) + 
       coord_fixed(1.3) + 
       scale_fill_gradient(
-        high = "red", 
-        low = "white", 
+        limits = limits,
+        high = muted("red"), 
+        low = "white",
         name = paste("Ratio: Scripts / Pop", drugname)
         ) +
       theme(legend.position = "bottom")
   } else {
     plot <- ggplot(data_fuzzy, aes(fill = drug_num))
     # the map = mapping makes sure ggplot knows what to use
-    plot + geom_map(aes(map_id = NAME), map = mapping) + 
+    plot + geom_map(aes(map_id = State), map = mapping) + 
       expand_limits(x=mapping$long, y=mapping$lat) + 
       coord_fixed(1.3) + 
       scale_fill_gradient(
+        limits = limits,
         high = "red", 
         low = "white", 
         name = paste("Total Scripts matching", drugname)
