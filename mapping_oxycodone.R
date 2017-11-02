aaff856a98262c46fb1b897ea21f782ddf774ed3
Jeramia Ory <jeramia.ory@gmail.com>
2017-11-02 10:16:46 -0500

    Changed it to a function, was also doing grepl wrong

--- mapping_oxycodone.R
+++ mapping_oxycodone.R
@@ -1,40 +1,54 @@
-library(tidycensus) 
-library(tidyverse)
+require(tidycensus) 
+require(tidyverse)
+require(maps)
 
 census_api_key("b851144a625a1ff4d8e04253547dde222768dd39", install = "TRUE")
 
 # Mapping Oxycontin Prescriptions
 
-# Read in the 2016 Medicaire Data
-all_data <- read_csv("State_Drug_Utilization_Data_2016.csv")
-# Make a simple data frame that just has the total OXYCODONE prescriptions
-# This misses a number of prescriptions!
-all_data <- all_data %>% rename_all(make.names)
-
-oxy_state <- all_data %>% 
-  select(State, Product.Name, Number.of.Prescriptions, Suppression.Used) %>% 
-  filter(Product.Name == "OXYCODONE", Suppression.Used == "false") %>% 
-  group_by(State) %>% summarise(oxy_num = sum(Number.of.Prescriptions))
-# The data is in two letter format, convert it to name
-# this converts DC and XX to NA
-colnames(oxy_state) <- tolower(colnames(oxy_state))
-oxy_state$State <-  state.name[match(oxy_state$State, state.abb)]
-state_2010 <- get_decennial(geography = "state", variables = "P0010001", year = "2010")
-oxy_state <- left_join(oxy_state, states, by = 'state')
-oxy_state$name <-  tolower(oxy_state$name)
-oxy_state$population <- as.numeric(oxy_state$population)
-oxy_state <- mutate(oxy_state, norm_oxy = oxy_num / population)
-#oxy_state <- oxy_state %>% filter(!is.na(State))
-
-# This tells ggplot how to draw our map later
-mapping <- map_data("state")
-# initial plot object, filled with numbr of oxy prescriptions
-plot <- ggplot(oxy_state, aes(fill = norm_oxy))
-# the map = mapping makes sure ggplot knows what to use
-plot + geom_map(aes(map_id = name), map = mapping) + 
-  expand_limits(x=map$long, y=map$lat) + 
-  coord_fixed(1.3) + scale_fill_gradient(high = "red", low = "blue", 
-                                         name = "Number of Prescriptions") +
-  theme(legend.position = "bottom")
+plot_drug <- function(dat, drugname, norm = TRUE) {
+  # take a data frame, search for a drug, and plot it, 
+  # either raw or normalized 
+  dat <- dat %>% rename_all(make.names)
+  data_fuzzy <- dat %>% 
+    select(State, Product.Name, Number.of.Prescriptions, Suppression.Used) %>% 
+    filter(grepl(pattern = drugname,x = Product.Name, ignore.case = TRUE), Suppression.Used == "false") %>% 
+    group_by(State) %>% summarise(drug_num = sum(Number.of.Prescriptions))
+  colnames(data_fuzzy) <- tolower(colnames(data_fuzzy))
+  data_fuzzy$state <-  state.name[match(data_fuzzy$state, state.abb)]
+  colnames(data_fuzzy)[1] <- "NAME"
+  state_2010 <- get_decennial(geography = "state", variables = "P0010001", year = "2010")
+  data_fuzzy <- left_join(data_fuzzy, state_2010, by = 'NAME')
+  data_fuzzy$NAME <-  tolower(data_fuzzy$NAME)
+  data_fuzzy <- data_fuzzy %>% filter(!is.na(NAME))
+  data_fuzzy$value <- as.numeric(data_fuzzy$value)
+  data_fuzzy <- mutate(data_fuzzy, drug_norm = drug_num / value)
+  mapping <- map_data("state")
+  if (norm) {
+    plot <- ggplot(data_fuzzy, aes(fill = drug_norm))
+    # the map = mapping makes sure ggplot knows what to use
+    plot + geom_map(aes(map_id = NAME), map = mapping) + 
+      expand_limits(x=mapping$long, y=mapping$lat) + 
+      coord_fixed(1.3) + 
+      scale_fill_gradient(
+        high = "red", 
+        low = "white", 
+        name = paste("Ratio: Scripts / Pop", drugname)
+        ) +
+      theme(legend.position = "bottom")
+  } else {
+    plot <- ggplot(data_fuzzy, aes(fill = drug_num))
+    # the map = mapping makes sure ggplot knows what to use
+    plot + geom_map(aes(map_id = NAME), map = mapping) + 
+      expand_limits(x=mapping$long, y=mapping$lat) + 
+      coord_fixed(1.3) + 
+      scale_fill_gradient(
+        high = "red", 
+        low = "white", 
+        name = paste("Total Scripts matching", drugname)
+        ) +
+      theme(legend.position = "bottom")
+  }
   
+}
 
