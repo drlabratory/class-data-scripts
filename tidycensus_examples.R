b5ca1f12d87ee7b67f85275475129f063b4eed45
Jeramia Ory <jeramia.ory@gmail.com>
2017-11-06 11:24:13 -0600

    Corrected the variable for acs data

--- tidycensus_examples.R
+++ tidycensus_examples.R
@@ -18,6 +18,8 @@ v2010 <- load_variables(2010, "sf1")
 state_2010 <- get_decennial(geography = "state", variables = "P0010001", year = "2010")
 
 # Make a dataframe of state level survey population data. Notice the count data is in 
-# a column called "estimate"
-state_2015 <- get_acs(geography = "state", variables = "B01003_001E", year = "2015")
+# a column called "estimate" Note that while the variable for population is actually
+# "B01003_001E" you can leave off the last "E" and it returns the estimate and the 
+# margin of error
+state_2015 <- get_acs(geography = "state", variables = "B01003_001", year = "2015")
 
